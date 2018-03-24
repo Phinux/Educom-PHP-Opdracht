0b6afbc67af2709e9d6d7609255057494e97f59f
Phinux <Marie-Anne.toonen@live.nl>
2018-03-24 20:51:18 +0100

    Opdracht 1

--- index.php
+++ index.php
@@ -5,3 +5,28 @@
  * Date: 24-3-2018
  * Time: 19:58
  */
+?>
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <title>Home</title>
+</head>
+<body>
+<header>
+    <h1>Home</h1>
+</header>
+<nav>
+    <a href='#'>Home</a>
+    <a href='about.html'>About</a>
+    <a href='contact.html'>Contact</a>
+</nav>
+<main>
+    <h2>Welkom</h2>
+    <p>Deze site is een opdracht van Educom als toelatingstest</p>
+</main>
+<footer>
+    <small>&copy; Copyright 2018, Marie-Anne Toonen</small>
+</footer>
+</body>
+</html>
