1a92d52ecf4fd3947fb7438a24d6b5d1f08371a7
Phinux <Marie-Anne.toonen@live.nl>
2018-03-24 22:39:23 +0100

    Opdracht 2 first draft

--- index.php
+++ index.php
@@ -11,22 +11,28 @@
 <head>
     <meta charset='UTF-8'>
     <title>Home</title>
+    <link rel="stylesheet" type="text/css" href='css/main.css'
 </head>
 <body>
-<header>
-    <h1>Home</h1>
-</header>
-<nav>
-    <a href='#'>Home</a>
-    <a href='about.html'>About</a>
-    <a href='contact.html'>Contact</a>
-</nav>
-<main>
-    <h2>Welkom</h2>
-    <p>Deze site is een opdracht van Educom als toelatingstest</p>
-</main>
-<footer>
-    <small>&copy; Copyright 2018, Marie-Anne Toonen</small>
-</footer>
+<!-- I use the bootstrap way to center the page-->
+<div class="container">
+    <header>
+        <h1>Home</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='about.html'>About</a></li>
+            <li><a href='contact.html'>Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <h2>Welkom</h2>
+        <p>Deze site is een opdracht van Educom als toelatingstest</p>
+    </main>
+    <footer>
+        <small>&copy; Copyright 2018, Marie-Anne Toonen</small>
+    </footer>
+</div>
 </body>
 </html>
